f9f8db24a376bdb1c9565f15e0dcc3b36874402a
Mike Sinnema <msinnema33@gmail.com>
2019-12-19 07:43:29 -0700

    having problems getting extends to initialize name, age, location

--- index.js
+++ index.js
@@ -115,9 +115,8 @@ class Lambdasian {
         this.location = location;
     }
     speak() {
-        return `Hello my name is ${this.name}, I am from ${this.location}.`
+        return `Hello my name is ${this.name}, I am from ${this.location}.`;
     }
-
 }
 
 /*
@@ -134,8 +133,19 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
-
+class Instructor extends Lambdasian {
+    constructor({ specialty, favLanguage, catchPhrase }) {
+        super({ specialty, favLanguage, catchPhrase });
+        this.specialty = specialty;
+        this.favLanguage = favLanguage;
+        this.catchPhrase = catchPhrase;
+    }
+    demo(subject) {
+        return `Today we are learning about ${subject}`;
+    }
+    grade(studentObj, subject) {
+        return `${this.student.name} receives a perfect score on ${subject}`;
+    }
 }
 
 /*
@@ -153,7 +163,22 @@ class Instructor {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {
+class Student extends Lambdasian {
+    constructor({ previousBackground, className, favSubjects }) {
+        super({ previousBackground, className, favSubjects });
+        this.previousBackground = previousBackground;
+        this.className = className;
+        this.favSubjects = favSubjects;
+    }
+    listSubjects() {
+        return `Loving ${this.favSubjects}`;
+    }
+    PRAssignment(subject) {
+        return `${student.name} has submitted a PR for ${subject}`;
+    }
+    sprintChallenge(subject) {
+        return `${student.name} has begun sprint challenge on ${subject}`;
+    }
 
 }
 
@@ -170,8 +195,18 @@ class Student {
         + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
-class ProjectManager {
-
+class ProjectManager extends Instructor {
+    constructor({ gradClassName, favInstructor }) {
+        super({ gradClassName, favInstructor });
+        this.gradClassName = gradClassName;
+        this.favInstructor = favInstructor;
+    }
+    standUp(channel) {
+        return `${this.name} announces to ${channel}, @channel standy times!`;
+    }
+    debugsCode(studentObj, subject) {
+        return `${this.name} debugs ${student.name}'s code on ${subject}`;
+    }
 }
 
 /*
